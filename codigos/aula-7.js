29ae5d006f8eb338ba95681774ed308608f991e1
MendesMaquinas <api.mendes@mendesmaquinas.com.br>
2023-09-07 14:10:21 -0300

    update relacionamento

--- codigos/aula-7.js
+++ codigos/aula-7.js
@@ -1,28 +1,36 @@
+const endereco = require("../models/endereco");
 const db = require("../models/index");
 const Pessoa = db.Pessoas;
 const Endereco = db.Enderecos;
 
 async function adicionarEndereco(endereco) {
-    const enderecoCriado = await Endereco.create(endereco);
-    console.log(enderecoCriado);
+  const enderecoCriado = await Endereco.create(endereco);
+  console.log(enderecoCriado);
 }
 
-async function procurarEnderecoPorId (id) {
-    const endereco = await Endereco.findOne({
-        where: {id},
-       
-    })
+async function procurarEnderecoPorId(id) {
+  let endereco = await Endereco.findOne({
+    where: { id },
+    raw: true,
+    include: [
+      {
+        model: Pessoa,
+      },
+    ],
+    nest: true,
+  });
 
-    console.log(endereco);
+  console.log(endereco);
 }
 
-//adicionarEndereco({
+/*
+adicionarEndereco({
     bairro: "Capão",
     estado: "SP",
     cidade: "Campinas",
     rua: "Rua Alcantara Nevais",
     numero: 403,
     pessoaId: 1,
-//})
+})*/
 
-procurarEnderecoPorId(1);
+procurarEnderecoPorId(2);
